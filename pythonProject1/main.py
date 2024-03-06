bd7de0f832f00952106c7495fef7ee613485df85
SAEI RANJBAR <s.ranjbar@samsung.com>
2024-03-06 11:08:21 -0800

    All the files I want to commit

---
+++ pythonProject1/main.py
@@ -0,0 +1,9 @@
+import turtle
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "r"
+
+print(table)
